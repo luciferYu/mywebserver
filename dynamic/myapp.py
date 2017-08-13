5fa7de43889d87e1bb810496ac3c879b2cc844cc
yuzhiyi <81784779@qq.com>
2017-08-13 15:17:01 +0800

    更改为使用装饰器来路由网址

--- dynamic/myapp.py
+++ dynamic/myapp.py
@@ -4,8 +4,20 @@
 import time
 import re
 import asyncio
+from functools import wraps
+
+
+def route(url,route_dict):
+    def set_func(func):
+        route_dict[url] = func
+        def call_func(*args, **kwargs):
+            result = func(*args, **kwargs)
+            return result
+        return call_func
+    return set_func
 
 class app(object):
+    route_dict = {}
     def __init__(self):
         self.g_templates_dir = './templates'
         self.g_dynamic_dir = './dynamic'
@@ -14,6 +26,8 @@ class app(object):
         self.content = None
         self.stock_info = '''<td>01</td><td>600028</td><td>中国石化</td><td>-1.15%</td><td>0.16%</td><td>6.02</td><td>6.04</td><td>20170812</td><td></td>'''
 
+
+    @route('/index.py',route_dict)
     def index(self,template_file):
         with open(template_file,'rb') as f:
             content = f.read()
@@ -23,6 +37,7 @@ class app(object):
         print(content)
         return content.encode('utf-8')
 
+    @route('/center.py',route_dict)
     def center(self,template_file):
         with open(template_file,'rb') as f:
             content = f.read()
@@ -30,32 +45,37 @@ class app(object):
 
 
     def app(self,environ,set_headers):
-        attr = environ['PATH_INFO'].replace('.py','')
-        if hasattr(app,attr[1:]):
-            file_name = self.g_templates_dir + attr + '.html'
-            print(file_name)
-            #self.content = self.index(file_name)
-            func = getattr(app,attr[1:])
-            print(func)
-            self.content = func(self,file_name)
-            #print(self.content)
-            header_status = '200 OK\r\n'
-            header_body = [('Content-Type','text/html')]
-            content_len = len(self.content)
-            header_body.append(('Content-Length',content_len))
-            #print('-' * 50)
-            #print(header_status,header_body)
-            #print('-' * 50)
-            #print(set_headers)
-            set_headers(header_status,header_body)
-            return self.content
-        else:
+        try:
+            #if environ['PATH_INFO'] in app.route_dict.keys():
+                file_name = environ['PATH_INFO']
+                print(file_name)
+                file_name = file_name.replace('.py','.html')
+
+                file_name = self.g_templates_dir + file_name
+                print(file_name)
+                #self.content = self.index(file_name)
+                func = app.route_dict[environ['PATH_INFO']]
+                print(func)
+                self.content = func(self,file_name)
+                print(self.content)
+                #print(self.content)
+                header_status = '200 OK\r\n'
+                header_body = [('Content-Type','text/html')]
+                content_len = len(self.content)
+                header_body.append(('Content-Length',content_len))
+                #print('-' * 50)
+                #print(header_status,header_body)
+                #print('-' * 50)
+                #print(set_headers)
+                set_headers(header_status,header_body)
+                return self.content
+        except Exception as e:
             with open('./templates/404.html', 'rb') as f:
                 content = f.read()
                 response_status = '404 Not Found\r\n'
                 response_header = [('Content_Length:',len(content))]
                 set_headers(response_status, response_header)
-            return content
+            return content + str(e).encode('gbk')
 
 
 
